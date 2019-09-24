3ec570396d1aa0f2231317b00a45d60b2dac6029
kyugao <solonot@163.com>
2019-09-24 15:26:39 +0800

    修改构造方法

--- src/main/java/com/founder/core/response/GenericResponse.java
+++ src/main/java/com/founder/core/response/GenericResponse.java
@@ -12,12 +12,12 @@ public class GenericResponse<T> extends BaseResponse {
     @ApiModelProperty(value = "返回结果", example = "Need further definition")
     protected T result;
 
-    public GenericResponse(ResponseCode responseCode) {
-        super(responseCode);
+    public GenericResponse(ResponseCode responseCode, String... params) {
+        super(responseCode, params);
     }
 
-    public GenericResponse(ResponseCode responseCode, T result) {
-        this(responseCode);
+    public GenericResponse(ResponseCode responseCode, T result, String... params) {
+        this(responseCode, params);
         this.result = result;
     }
 
@@ -27,5 +27,11 @@ public class GenericResponse<T> extends BaseResponse {
 
     public void setResult(T result) {
         this.result = result;
+    }
+
+    public static void main(String[] args) {
+        String[] params = new String[]{"param1", "param2"};
+        GenericResponse resp = new GenericResponse<String>(ResponseCode.ParamError, "result", params);
+        System.out.println(resp);
     }
 }
